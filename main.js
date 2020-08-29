bf7b98b785c4025ac435ba19202bf2d775201447
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-08-29 13:09:50 -0500

    working on adventure button still

--- main.js
+++ main.js
@@ -91,58 +91,7 @@ const dinoArray = [
   }
 ];
 
-const adventureArray = [
-  {
-    id: "adventure1",
-    title: "BRAWL",
-    healthHit: 50,
-  },
-  {
-    id: "adventure2",
-    title: "Cave exploration",
-    healthHit: 10,
-  },
-  {
-    id: "adventure3",
-    title: "Ropes course",
-    healthHit: 13,
-  },
-  {
-    id: "adventure4",
-    title: "Playing in traffic",
-    healthHit: 3,
-  },
-  {
-    id: "adventure5",
-    title: "Baking",
-    healthHit: 70,
-  },
-  {
-    id: "adventure6",
-    title: "Welding",
-    healthHit: 4,
-  },
-  {
-    id: "adventure7",
-    title: "Underwater Basket Weaving",
-    healthHit: 99,
-  },
-  {
-    id: "adventure8",
-    title: "Surfing",
-    healthHit: 39,
-  },
-  {
-    id: "adventure9",
-    title: "Fishing",
-    healthHit: 23,
-  },
-  {
-    id: "adventure10",
-    title: "Shot from a cannon",
-    healthHit: 60,
-  },
-];
+
 
 
 
@@ -224,9 +173,6 @@ const petButton = (index, item, array) => {
   });
 };
 
-const randoAdventure = (array) => {
-  return array[Math.floor(Math.random() * array.length)];
-}
 
 
 
