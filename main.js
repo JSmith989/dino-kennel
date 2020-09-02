6ddd6f3b741d141ecd793bb863789148ec767e7a
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-09-02 00:25:45 -0500

    adventures logged in info working

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const dinoArray = [
     owner: 'Jacob',
     adventures: [],
     health: 92,
-    imageUrl: 'https://www.fieldandstream.com/resizer/8xkluKAxQZsEHJKj6qwyU0mLhTo=/760x448/filters:focal(458x270:459x271)/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/TQFN3CD5DAEM4DL2ACD42ZJ5E4.png'
+    imageUrl:
+      'https://www.fieldandstream.com/resizer/8xkluKAxQZsEHJKj6qwyU0mLhTo=/760x448/filters:focal(458x270:459x271)/arc-anglerfish-arc2-prod-bonnier.s3.amazonaws.com/public/TQFN3CD5DAEM4DL2ACD42ZJ5E4.png',
   },
   {
     id: 'dino2',
@@ -17,7 +18,7 @@ const dinoArray = [
     owner: 'Jacob',
     adventures: [],
     health: 1,
-    imageUrl: 'https://i.ebayimg.com/images/g/61UAAOSweNpdmtI2/s-l640.png'
+    imageUrl: 'https://i.ebayimg.com/images/g/61UAAOSweNpdmtI2/s-l640.png',
   },
   {
     id: 'dino3',
@@ -27,7 +28,8 @@ const dinoArray = [
     owner: 'Jacob',
     adventures: [],
     health: 0,
-    imageUrl: 'https://cdn.mos.cms.futurecdn.net/owYTb9X5fKpeBhgiaxD73b-320-80.jpg'
+    imageUrl:
+      'https://cdn.mos.cms.futurecdn.net/owYTb9X5fKpeBhgiaxD73b-320-80.jpg',
   },
   {
     id: 'dino4',
@@ -37,7 +39,8 @@ const dinoArray = [
     owner: 'Abbey',
     adventures: [],
     health: 100,
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSF2dDXzeJS-XOVA-JdrWqAMQ5xNPBwuL87IA&usqp=CAU'
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSF2dDXzeJS-XOVA-JdrWqAMQ5xNPBwuL87IA&usqp=CAU',
   },
   {
     id: 'dino5',
@@ -47,7 +50,8 @@ const dinoArray = [
     owner: 'Dr. T',
     adventures: [],
     health: 75,
-    imageUrl: 'https://cdn1.bigcommerce.com/n-yp39j5/ujq6o/products/1060/images/2390/Papo_Spinosaurus_2019_DansDinosaurs__69805.1552618774.1280.1280.jpg?c=2'
+    imageUrl:
+      'https://cdn1.bigcommerce.com/n-yp39j5/ujq6o/products/1060/images/2390/Papo_Spinosaurus_2019_DansDinosaurs__69805.1552618774.1280.1280.jpg?c=2',
   },
   {
     id: 'dino6',
@@ -57,7 +61,8 @@ const dinoArray = [
     owner: 'Dr. T',
     adventures: [],
     health: 55,
-    imageUrl: 'https://vignette.wikia.nocookie.net/dinosaurs/images/2/2b/TalarurusInfobox.png/revision/latest/scale-to-width-down/340?cb=20150512165226'
+    imageUrl:
+      'https://vignette.wikia.nocookie.net/dinosaurs/images/2/2b/TalarurusInfobox.png/revision/latest/scale-to-width-down/340?cb=20150512165226',
   },
   {
     id: 'dino7',
@@ -67,7 +72,8 @@ const dinoArray = [
     owner: 'Abbey',
     adventures: [],
     health: 0,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/81Wsvp2M7iL._AC_SX425_.jpg'
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/81Wsvp2M7iL._AC_SX425_.jpg',
   },
   {
     id: 'dino8',
@@ -77,7 +83,7 @@ const dinoArray = [
     owner: 'Jacob',
     adventures: [],
     health: 10,
-    imageUrl: 'https://images.dinosaurpictures.org/3_pterodactyl_63be.jpg'
+    imageUrl: 'https://images.dinosaurpictures.org/3_pterodactyl_63be.jpg',
   },
   {
     id: 'dino9',
@@ -87,13 +93,63 @@ const dinoArray = [
     owner: 'Dr. T',
     adventures: [],
     health: 22,
-    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTOdrC7hlvBawFQ7g8vgwHcfQphX5WfeN2bth0dvc4M2oxNGdSD'
-  }
+    imageUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTOdrC7hlvBawFQ7g8vgwHcfQphX5WfeN2bth0dvc4M2oxNGdSD',
+  },
 ];
 
-
-
-
+const adventureArray = [
+  {
+    id: 'adventure1',
+    title: 'BRAWL',
+    healthHit: 50,
+  },
+  {
+    id: 'adventure2',
+    title: 'Cave exploration',
+    healthHit: 10,
+  },
+  {
+    id: 'adventure3',
+    title: 'Ropes course',
+    healthHit: 13,
+  },
+  {
+    id: 'adventure4',
+    title: 'Playing in traffic',
+    healthHit: 3,
+  },
+  {
+    id: 'adventure5',
+    title: 'Baking',
+    healthHit: 70,
+  },
+  {
+    id: 'adventure6',
+    title: 'Welding',
+    healthHit: 4,
+  },
+  {
+    id: 'adventure7',
+    title: 'Underwater Basket Weaving',
+    healthHit: 99,
+  },
+  {
+    id: 'adventure8',
+    title: 'Surfing',
+    healthHit: 39,
+  },
+  {
+    id: 'adventure9',
+    title: 'Fishing',
+    healthHit: 23,
+  },
+  {
+    id: 'adventure10',
+    title: 'Shot from a cannon',
+    healthHit: 60,
+  },
+];
 
 const makeDinoCards = (item, index) => {
   let domString = `
@@ -116,7 +172,12 @@ const makeDinoCards = (item, index) => {
             <p>Health: ${item.health}%</p> <div class="progress">
              <div id="prog-${index}" class="progress-bar progress-bar-striped" role="progressbar" style="width: ${item.health}%" aria-valuenow="${item.health}" aria-valuemin="0" aria-valuemax="100"></div>
            </div>
+           <div class="adventLog">
+           <h5 class="date">Adventure Log</h5>
+          <p id="adventureLog-${index}"></p>
+        </div>
       </div>
+        
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
@@ -139,35 +200,36 @@ const makeDinoCards = (item, index) => {
      </div>
    </div>
 </div>
-  `
-  return domString
-}
+  `;
+  return domString;
+};
 
 const deleteDino = (index, array) => {
   $(`#delete-${index}`).click(() => {
-    array.splice(index, 1)
-    dinoCards(array)
-  })
+    array.splice(index, 1);
+    dinoCards(array);
+  });
 };
-
 
 const feedButton = (index, item, array) => {
   $(`#feed-${index}`).click(() => {
-    if(item.health > 0){
-      item.health += 7;}
-      if(item.health > 100){
-        return window.alert("Too much energy, I need an adventure")
-      }
+    if (item.health > 0) {
+      item.health += 7;
+    }
+    if (item.health > 100) {
+      return window.alert('Too much energy, I need an adventure');
+    }
     dinoCards(array);
   });
 };
 
 const petButton = (index, item, array) => {
   $(`#pet-${index}`).click(() => {
-    if(item.health > 0){
-    item.health += 3;}
-    if(item.health > 100){
-      return window.alert("Too much energy, I need an adventure")
+    if (item.health > 0) {
+      item.health += 3;
+    }
+    if (item.health > 100) {
+      return window.alert('Too much energy, I need an adventure');
     }
     dinoCards(array);
   });
@@ -175,38 +237,56 @@ const petButton = (index, item, array) => {
 
 const adventureButton = (index, item, array) => {
   $(`#adventure-${index}`).click(() => {
-    if(item.health > 0){
-    item.health -= 3;}
+    if (item.health > 0) {
+      item.health -= 8;
+
+      item.adventures.push({
+        timeStamp: new Date(),
+        adventure: randoAdventure(adventureArray).title,
+      });
+    };
     dinoCards(array);
   });
 };
 
+const randoAdventure = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const adventureLog = (item, index) => {
+  item.adventures.forEach((adventure, i) => {
+    $(`#adventureLog-${index}`).append(`
+      <body>
+          <p>${adventure.timeStamp}</p>
+          <p><b>Adventure Type:</b> ${adventure.adventure}</p>
+      </body>
+      `);
+  });
+};
 
 const dinoCards = (array) => {
-  $("#dino-kennel").html("");
-  $("#dino-hospital").html("");
-  $("#dino-dead").html("");
+  $('#dino-kennel').html('');
+  $('#dino-hospital').html('');
+  $('#dino-dead').html('');
   dinoArray.forEach((item, index) => {
-    if(item.health >= 35){
-    $("#dino-kennel").append(makeDinoCards(item, index));
-  } else if(item.health < 35 && item.health > 0) {
-    $("#dino-hospital").append(makeDinoCards(item, index));
-  } else {
-    $("#dino-dead").append(makeDinoCards(item, index));
-  }
-  petButton(index, item, array)
-  adventureButton(index, item, array)
-  feedButton(index, item, array)
-  deleteDino(index, array)
-    });
-  };
+    if (item.health >= 35) {
+      $('#dino-kennel').append(makeDinoCards(item, index));
+    } else if (item.health < 35 && item.health > 0) {
+      $('#dino-hospital').append(makeDinoCards(item, index));
+    } else {
+      $('#dino-dead').append(makeDinoCards(item, index));
+    }
+    adventureLog(item, index);
+    petButton(index, item, array);
+    adventureButton(index, item, array);
+    feedButton(index, item, array);
+    deleteDino(index, array);
+  });
+};
 
-
-
-  
 const dinoForm = () => {
-    $("#addDino").click(() => {
-        $("#dinoForm").html(`
+  $('#addDino').click(() => {
+    $('#dinoForm').html(`
         <form id="your-dino">
               <div class="row">
                 <div class="col">
@@ -227,44 +307,41 @@ const dinoForm = () => {
               </div>
               <input type="button" id="submitDino" class="btn btn-info" value="Add My Dino"></input>
             </form>
-        `)
-        addDino();
-    })
+        `);
+    addDino();
+  });
 };
 
 const addDino = () => {
-    $("#submitDino").click(() => {
-        let dinoInfo = {};
-        dinoInfo.name = $("#dinoName").val();
-        dinoInfo.type = $("#dinoType").val();
-        dinoInfo.age = $("#dinoAge").val();
-        dinoInfo.owner = $("#dinoOwner").val();        
-        dinoInfo.adventures = [];
-        dinoInfo.health = 50;     
-        dinoInfo.imageUrl = $("#dinoIMG").val();        
-        dinoArray.push(dinoInfo);
-        dinoCards(dinoArray);
-        clearForm();
-    });
-    $("#addDino").click(() => {
-        $("#dinoForm").toggle()
-    })
+  $('#submitDino').click(() => {
+    let dinoInfo = {};
+    dinoInfo.name = $('#dinoName').val();
+    dinoInfo.type = $('#dinoType').val();
+    dinoInfo.age = $('#dinoAge').val();
+    dinoInfo.owner = $('#dinoOwner').val();
+    dinoInfo.adventures = [];
+    dinoInfo.health = 50;
+    dinoInfo.imageUrl = $('#dinoIMG').val();
+    dinoArray.push(dinoInfo);
+    dinoCards(dinoArray);
+    clearForm();
+  });
+  $('#addDino').click(() => {
+    $('#dinoForm').toggle();
+  });
 };
 
-
 const clearForm = () => {
-    $('#dinoName').val('');
-    $('#dinoType').val('');
-    $('#dinoAge').val('');
-    $('#dinoOwner').val('');
-    $('#dinoIMG').val('');
-}
-
+  $('#dinoName').val('');
+  $('#dinoType').val('');
+  $('#dinoAge').val('');
+  $('#dinoOwner').val('');
+  $('#dinoIMG').val('');
+};
 
 const init = () => {
-    dinoCards(dinoArray);
-    dinoForm();
-    // randoAdventure(adventureArray)
-  };
-  
-  init();
+  dinoCards(dinoArray);
+  dinoForm();
+};
+
+init();
